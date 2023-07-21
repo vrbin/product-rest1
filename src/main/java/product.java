4312a3a8d77c0d9fcf210c5e62c9f13bdf6359f2
Vrba Martin <Martin.Vrba@t-mobile.cz>
2023-07-21 11:18:18 +0200

    cviceni 4-1

---
+++ src/main/java/product.java
@@ -0,0 +1,13 @@
+public class Product {
+    private long id;
+    private String name;
+    private double price;
+    private String description;
+
+    public Product(long id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
